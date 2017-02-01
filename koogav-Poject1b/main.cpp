12b43c79944cc7d1c06ee412f3e2435d2aabb5db
pgavelek <patriciagavelek@Patricias-MacBook-Pro.local>
2017-02-01 02:14:20 -0500

    Initial Commit

---
+++ koogav-Poject1b/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  koogav-Poject1b
+//
+//  Created by Patricia Gavelek on 2/1/17.
+//  Copyright © 2017 Patricia Gavelek. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
